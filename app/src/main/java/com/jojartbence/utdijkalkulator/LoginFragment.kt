4683f2f10df73a3dd96d2f2e621f56b548d7a548
jojartbence <jojart.bence96@gmail.com>
2020-03-22 21:23:06 +0100

    Firebase auth works

--- app/src/main/java/com/jojartbence/utdijkalkulator/LoginFragment.kt
+++ app/src/main/java/com/jojartbence/utdijkalkulator/LoginFragment.kt
@@ -1,32 +1,138 @@
 package com.jojartbence.utdijkalkulator
 
+import android.content.Context
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.login_fragment.*
 
 
 class LoginFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = LoginFragment()
+    private val viewModel by lazy { ViewModelProviders.of(this)[LoginViewModel::class.java] }
+
+    lateinit var navController: NavController
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        val loginResultObserver = Observer<Boolean?> {
+            hideProgressBar()
+            when(it) {
+                true -> {
+                    navController.navigate(R.id.action_loginFragment_to_siteListFragment)
+                }
+                false -> {
+                    Toast.makeText(activity, "Log in failed: ${viewModel.errorMessage}", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        val signupResultObserver = Observer<Boolean?> {
+            hideProgressBar()
+            when(it) {
+                true -> {
+                    navController.navigate(R.id.action_loginFragment_to_siteListFragment)
+                }
+                false -> {
+                    Toast.makeText(activity, "Sign up failed: ${viewModel.errorMessage}", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        viewModel.loginResult.observe(this, loginResultObserver)
+        viewModel.signupResult.observe(this, signupResultObserver)
+
+        super.onCreate(savedInstanceState)
     }
 
-    private lateinit var viewModel: LoginViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        setHasOptionsMenu(true)
+
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.login_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(LoginViewModel::class.java)
-        // TODO: Use the ViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+
+        // viewModel.skipIfAlreadyLoggedIn(activity!!.applicationContext)
+
+        logIn.setOnClickListener {
+            logIn()
+        }
+
+        signUp.setOnClickListener {
+            signUp()
+        }
     }
 
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                return viewModel.closeApp(activity)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+    private fun logIn() {
+        val email = email.text.toString()
+        val password = password.text.toString()
+        if (email == "" || password == "") {
+            Toast.makeText(activity, "Please provide email + password", Toast.LENGTH_SHORT).show()
+        } else {
+            hideKeyboard()
+            showProgressBar()
+            viewModel.doLogin(email, password, activity!!.applicationContext)
+        }
+    }
+
+
+    private fun signUp() {
+        val email = email.text.toString()
+        val password = password.text.toString()
+        if (email == "" || password == "") {
+            Toast.makeText(activity, "Please provide email + password", Toast.LENGTH_SHORT).show()
+        }
+        else {
+            hideKeyboard()
+            showProgressBar()
+            viewModel.doSignUp(email, password, activity!!.applicationContext)
+        }
+    }
+
+
+    private fun showProgressBar() {
+        progressBar.visibility = View.VISIBLE
+    }
+
+
+    private fun hideProgressBar() {
+        progressBar.visibility = View.INVISIBLE
+    }
+
+
+    fun hideKeyboard() {
+        val imm = activity!!.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view!!.windowToken, 0)
+    }
 }
--- app/src/main/java/com/jojartbence/utdijkalkulator/LoginViewModel.kt
+++ app/src/main/java/com/jojartbence/utdijkalkulator/LoginViewModel.kt
@@ -1,7 +1,77 @@
 package com.jojartbence.utdijkalkulator
 
+import android.content.Context
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
 
-class LoginViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class LoginViewModel: ViewModel() {
+
+    private var auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+    val loginResult: MutableLiveData<Boolean?> by lazy {
+        MutableLiveData<Boolean?>(null)
+    }
+
+    val signupResult: MutableLiveData<Boolean?> by lazy {
+        MutableLiveData<Boolean?>(null)
+    }
+
+    var errorMessage: String? = null
+
+
+    fun doLogin(email: String, password: String, context: Context) {
+
+        auth.signInWithEmailAndPassword(email, password).addOnSuccessListener {
+            initRepository(email, context, { loginResult.value = true })
+        }.addOnFailureListener {
+            handleAuthenticationFailure(context, email, it)
+        }
+    }
+
+
+    fun doSignUp(email: String, password: String, context: Context) {
+
+        auth.createUserWithEmailAndPassword(email, password).addOnSuccessListener {
+            initRepository(email, context, { loginResult.value = true })
+        }.addOnFailureListener {
+            handleAuthenticationFailure(context, email, it)
+        }
+    }
+
+
+    fun initRepository(email: String, context: Context, onSuccess: () -> Unit) {
+      //  SiteRepository.createDatabase(context, email)
+      //  SiteRepository.fetchSites ( onSuccess )
+        onSuccess()
+    }
+
+
+    fun closeApp(activity: FragmentActivity?): Boolean {
+        activity?.finishAndRemoveTask()
+        return true
+    }
+
+
+    private fun handleAuthenticationFailure(context: Context, email: String, it: Exception) {
+        when (it.message?.contains("internal")) {
+            false -> {
+                errorMessage = it.message
+                loginResult.value = false
+            }
+            true -> {
+             //   SiteRepository.createDatabaseUsingBackup(context, email)
+             //   SiteRepository.fetchSites { loginResult.value = true }
+                loginResult.value = true
+            }
+        }
+    }
+
+
+    fun skipIfAlreadyLoggedIn(context: Context) {
+        auth.currentUser?.email?.let {
+            initRepository(it, context, { loginResult.value = true })
+        }
+    }
 }
