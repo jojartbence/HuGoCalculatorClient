0d6b201abfe50a40c76418f9cb657a4f27891fc0
jojartbence <jojart.bence96@gmail.com>
2020-03-29 17:09:55 +0200

    TruckDataModel has an id now (it will be used as a firebase it, so its a bit messy now, refactor needed later)

--- app/src/main/java/com/jojartbence/model/TruckDataModel.kt
+++ app/src/main/java/com/jojartbence/model/TruckDataModel.kt
@@ -5,6 +5,7 @@ data class TruckDataModel (
     ) {
 
     private var distancePerDay: Array<IntArray> = Array(2) {IntArray(3)}
+    lateinit var id: String // TODO: it shouldnt be used as a firebase id
 
     enum class RoadType {
         MOTORWAY, MAINROAD
