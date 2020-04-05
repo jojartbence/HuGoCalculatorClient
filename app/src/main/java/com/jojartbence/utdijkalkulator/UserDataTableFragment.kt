d1cb79580c368590fef1b45e5b8944c8c2345cb0
jojartbence <jojart.bence96@gmail.com>
2020-04-05 16:41:26 +0200

    Can add truck to firebase db

--- app/src/main/java/com/jojartbence/utdijkalkulator/UserDataTableFragment.kt
+++ app/src/main/java/com/jojartbence/utdijkalkulator/UserDataTableFragment.kt
@@ -6,9 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.jojartbence.helpers.LicensePlateNumberValidityChecker
 import kotlinx.android.synthetic.main.user_data_table_fragment.*
 
 
@@ -37,5 +39,18 @@ class UserDataTableFragment : Fragment() {
         recyclerView.layoutManager = LinearLayoutManager(activity!!.applicationContext)
 
         recyclerView.adapter?.notifyDataSetChanged()
+
+        addTruckButton.setOnClickListener { addTruck() }
+    }
+
+
+    private fun addTruck() {
+
+        if (LicensePlateNumberValidityChecker.isValidLicensePlateNumber(newTruckLicensePlateText.text.toString())) {
+            viewModel.addNewTruck(newTruckLicensePlateText.text.toString())
+        } else {
+            Toast.makeText(activity, "Format is wrong", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
--- app/src/main/java/com/jojartbence/utdijkalkulator/UserDataTableViewModel.kt
+++ app/src/main/java/com/jojartbence/utdijkalkulator/UserDataTableViewModel.kt
@@ -2,6 +2,7 @@ package com.jojartbence.utdijkalkulator
 
 import androidx.lifecycle.ViewModel
 import com.jojartbence.model.TruckModel
+import com.jojartbence.repository.TruckRepository
 
 class UserDataTableViewModel : ViewModel() {
 
@@ -10,5 +11,10 @@ class UserDataTableViewModel : ViewModel() {
         val truck2 = TruckModel("CDE-678")
 
         return listOf(truck1, truck2)
+    }
+
+    fun addNewTruck(licensePlateNumber: String) {
+        val newTruck = TruckModel(licensePlateNumber)
+        TruckRepository.create(newTruck)
     }
 }
